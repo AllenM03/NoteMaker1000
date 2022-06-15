e4d90d2b09f296eabda0f3427ff6f26e3ddee817
Allen <almcbroombey@hotmail.com>
2022-06-15 15:59:28 -0400

    edited routes files

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -9,3 +9,36 @@ const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
+
+  const readAndAppend = (content, file) => {
+    // first read the file
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // if there's no error, convert JSON string to object
+        const parsedData = JSON.parse(data);
+        // so that the added content can be pushed into the array
+        parsedData.push(content);
+        // store the newly created array to the given file
+        writeToFile(file, parsedData);
+        console.info(`\nData added to ${file}`);
+      }
+    });
+  };
+
+  const deleteIdFromFile = (contentId, file) => {
+    readFromFile(file)
+      // convert JSON string to object so we could use array functions in it
+      .then((data) => JSON.parse(data))
+      .then((dataObj) => {
+        // filter the array object so that we get all objects except for the one with selected ID
+      const filteredData = dataObj.filter((item) => item.id !== contentId);
+
+      // write to the file the filteredData
+      writeToFile(file, filteredData);
+      console.info(`\nItem ${contentId} has been deleted from ${file}`);
+    });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend, deleteIdFromFile };
--- Develop/server.js
+++ Develop/server.js
@@ -23,6 +23,6 @@ app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
-app.listen(PORT, () =>
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
-);
+});
