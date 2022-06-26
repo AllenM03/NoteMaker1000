07e0e4f744b0f83b5a2fbb47e2466ee7b584f555
Allen <almcbroombey@hotmail.com>
2022-06-26 05:17:46 -0400

    Continue adding GET NOTES and POST NOTES to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,34 +3,34 @@
 // const api = require("./routes/index.js");
 
 // use either environment PORT or 3001 (for testing)
-const PORT = process.env.PORT || 3001;
-const app = express();
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
 // Middleware for parsing JSON data
-app.use(express.json());
-// Middleware for using all files in the public folder
-app.use(express.static("public"));
-// Added index.js that acts as an overall router
-app.use("/api", api);
+// app.use(express.json());
+// // Middleware for using all files in the public folder
+// app.use(express.static("public"));
+// // Added index.js that acts as an overall router
+// app.use("/api", api);
 
-// GET Route for notes.html
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
-);
+// // GET Route for notes.html
+// app.get("/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "./public/notes.html"))
+// );
 
-// route to direct users to index.html
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-);
+// // route to direct users to index.html
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "./public/index.html"))
+// );
 
-app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`)
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening at http://localhost:${PORT}`)
+// });
 
 
-///////////////////////////////////////
-//////////////////Git branch develop start to troubleshoot server.js
-
+                        ///////////////////////////////////////
+///////////////   Git branch develop start to troubleshoot server.js   ////////////
+                        //////////////////////////////////////
 
 
 
@@ -53,10 +53,38 @@ const notes = require("./db/db.json");
 app.use(express.urlencoded({ extended: true }));
 // Parses incoming JSON data
 app.use(express.json());
-// Uses public/ folder for Express
+// Middleware for using all files in the public folder
 app.use(express.static("public"));
 
 // GET Route for retrieving all saved notes and returning all saved notes as JSON. 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
+});
+
+
+
+// POST to receive a new note to
+
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = uuid.v1();
+  notes.push(newNote);
+
+  console.log(notes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+  });
+  res.json(notes);
+});
+
+////GET notes dir returns the notes.html file
+
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
+
+
+// listens for console.log
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
 });
